b8b28236a64fe7a2a59e2d444cd98a259c3e0065
Jereco Catingub <jerecocatingub216@gmail.com>
2026-05-07 09:58:30 +0800

    feat: show playoff round and game in series in game info card

--- client/src/pages/admin/games/game-details/summary/GameInfoCard.tsx
+++ client/src/pages/admin/games/game-details/summary/GameInfoCard.tsx
@@ -63,6 +63,18 @@ const GameInfoCard = ({ game, busy, updateGameInfo }: Props) => {
             data={game.venue ?? null}
             full
           />
+          {game.playoff_round != null && (
+            <InfoItem
+              label="Round"
+              data={String(game.playoff_round)}
+            />
+          )}
+          {game.game_number_in_series != null && (
+            <InfoItem
+              label="Game in Series"
+              data={String(game.game_number_in_series)}
+            />
+          )}
           {game.game_number != null && (
             <InfoItem
               label="Game #"
