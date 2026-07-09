6f5aad9c272cc350dcb3f26aa15aca8cf4745ba6
Jereco Catingub <jerecocatingub216@gmail.com>
2026-07-09 20:18:41 +0800

    Adjust mobile section and layout spacing

--- client/src/components/Section/Section.tsx
+++ client/src/components/Section/Section.tsx
@@ -1,2 +1,20 @@
-export { default } from '@jerecocc/tracker-ui/components/Section/Section';
+import { forwardRef, type ComponentPropsWithoutRef, type ElementRef } from 'react';
+import BaseSection from '@jerecocc/tracker-ui/components/Section/Section';
+import styles from './Section.module.scss';
+
+type SectionProps = ComponentPropsWithoutRef<typeof BaseSection>;
+
+const Section = forwardRef<ElementRef<typeof BaseSection>, SectionProps>(
+  ({ className, ...props }, ref) => (
+    <BaseSection
+      ref={ref}
+      className={[styles.sectionRoot, className].filter(Boolean).join(' ')}
+      {...props}
+    />
+  ),
+);
+
+Section.displayName = 'Section';
+
+export default Section;
 export * from '@jerecocc/tracker-ui/components/Section/Section';
